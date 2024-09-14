7b2b58c6d0d55e9aa458ee0199f80a4773891832
Gonzalo Del Castello <delcastello.gonzalo@sanluis.edu.ar>
2024-09-14 12:53:47 -0300

    Initial commit

---
+++ src/main/java/entidades/Producto.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+
+public class Producto {
+
+    public String categoria;
+    public String nombre;
+    public double precio;
+
+    public Producto(String categoria, String nombre, double precio) {
+        this.categoria = categoria;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    
+}
